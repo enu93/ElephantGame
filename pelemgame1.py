dbea33f8df63ff13e09117ae80d7863df90e16a0
Innu Thapa <innuthapa93@gmail.com>
2013-08-30 11:45:02 +0545

    add pelemgame1 file to show move of mouse cursor

---
+++ pelemgame1.py
@@ -0,0 +1,30 @@
+bif="bg.jpg"
+mif="ball.png"
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+screen=pygame.display.set_mode((250, 190), 0, 32)
+background=pygame.image.load(bif).convert()
+mouse_c=pygame.image.load(mif).convert_alpha()
+
+x, y = 0, 0
+movex, movey = 0, 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.blit(background, (0, 0))
+
+    x,y = pygame.mouse.get_pos()
+    x -= mouse_c.get_width()/2
+    y -= mouse_c.get_height()/2
+
+    screen.blit(mouse_c, (x, y))
+
+    pygame.display.update()
