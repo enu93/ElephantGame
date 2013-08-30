7ac7328dd612da476eb3663218a4f2df331eef9a
Innu Thapa <innuthapa93@gmail.com>
2013-08-30 12:59:40 +0545

    draw line and ellipse

--- drawing_shapes.py
+++ drawing_shapes.py
@@ -4,15 +4,21 @@ from pygame.locals import *
 pygame.init()
 
 screen = pygame.display.set_mode((640, 350), 0, 32)
+color = (255, 255, 0)
 
 # polygon ponts
-points = [(20, 120), (140, 140), (110,30)]
-color = (255, 255, 0)
+points = [(20, 120), (140, 140), (10,30)]
 
 # circle points
 position = (300, 176)
 radius = (60)
-color = (230, 170, 0)
+
+# ellipse points
+rectangle = (140, 180, 50, 190)
+
+# draw line
+pos1 = (20, 20)
+pos2 = (150, 134)
 
 while True:
     for event in pygame.event.get():
@@ -30,6 +36,12 @@ while True:
     # draw circle
     pygame.draw.circle(screen, color, position, radius)
 
+    # draw ellipse
+    pygame.draw.ellipse(screen, color, rectangle)
+
+    # draw line
+    pygame.draw.line(screen, color, pos1, pos2, 10)
+    
     screen.unlock()
 
     pygame.display.update()
